98803dccdb7c2eec8201943fa21402dea1eef230
Ryan Graham <ryangrahamwpb@gmail.com>
2020-01-30 18:43:31 -0500

    Refactor js.

--- script.js
+++ script.js
@@ -2,19 +2,26 @@ let form = document.querySelector("#parking-form");
 let inputs = document.querySelectorAll("input");
 let fields = document.querySelectorAll(".input-field");
 let inputName = document.querySelector("#name");
+let cvv = document.querySelector("#cvv");
+console.log(cvv);
 
 form.addEventListener("submit", function(event) {
   event.preventDefault();
   console.log(event);
   console.log(event.target);
   for (let i = 0; i < fields.length; i++) {
-    if (inputs[i].value === "") {
+    if (inputs[i].value.length === 0) {
       fields[i].classList.add("input-invalid");
       let span = document.createElement("span");
       fields[i].appendChild(span);
+      span.classList.add("error");
       span.innerHTML = "Not valid form entry.";
-    } else if (inputs[i].value.length >= 2) {
-      fields[i].classList.add("input-valid");
-    }
+    } else fields[i].classList.add("input-valid");
   }
 });
+
+// formData
+// let data = new FormData(document.querySelector("#parking-form"));
+// for (let val of data.values()) {
+//   console.log(val);
+// }
