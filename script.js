147850b43150b600b56c32f82e17186ccd16df33
Ryan Graham <ryangrahamwpb@gmail.com>
2020-01-30 14:09:34 -0500

    Add validations for input feilds.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Add validation!');
+let form = document.querySelector("#parking-form");
+let inputs = document.querySelectorAll("input");
+let fields = document.querySelectorAll(".input-field");
+let inputName = document.querySelector("#name");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  console.log(event);
+  console.log(event.target);
+  for (let i = 0; i < fields.length; i++) {
+    if (inputs[i].value === "") {
+      fields[i].classList.add("input-invalid");
+      let span = document.createElement("span");
+      fields[i].appendChild(span);
+      span.innerHTML = "Not valid form entry.";
+    } else if (inputs[i].value.length >= 2) {
+      fields[i].classList.add("input-valid");
+    }
+  }
+});
